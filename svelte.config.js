9bd75b655c26ec4b5c5615b258d6975b35070e04
Uwan Maharaya <141812391+uwangraph@users.noreply.github.com>
2026-04-20 10:24:39 +0700

    Update svelte.config.js
    jadi static

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
-		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+		runes: ({ filename }) =>
+			filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 	},
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		prerender: {
+			entries: ['*']
+		}
+	}
 };
 
 export default config;

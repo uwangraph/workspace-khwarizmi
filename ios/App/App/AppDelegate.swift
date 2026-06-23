2e602bf61cedfd0490e9364caf848bdcd49dc891
Uwan Maharaya <141812391+uwangraph@users.noreply.github.com>
2026-06-23 17:36:30 +0700

    fix(ios): sederhanakan AppDelegate - hapus notifikasi Capacitor yang tidak valid

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -3,12 +3,11 @@ import Capacitor
 import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UNUserNotificationCenter.current().delegate = self
         return true
     }
 
@@ -26,32 +25,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
             name: .capacitorDidFailToRegisterForRemoteNotifications,
             object: error
         )
-    }
-
-    // MARK: - UNUserNotificationCenterDelegate
-
-    // Tampilkan notifikasi (banner + suara + badge) meskipun app sedang buka
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        willPresent notification: UNNotification,
-        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
-    ) {
-        completionHandler([.banner, .sound, .badge])
-    }
-
-    // Teruskan tap notifikasi ke Capacitor (agar JS bisa handle navigasi)
-    func userNotificationCenter(
-        _ center: UNUserNotificationCenter,
-        didReceive response: UNNotificationResponse,
-        withCompletionHandler completionHandler: @escaping () -> Void
-    ) {
-        let userInfo = response.notification.request.content.userInfo
-        NotificationCenter.default.post(
-            name: Notification.Name.capacitorDidReceiveNotification,
-            object: nil,
-            userInfo: userInfo
-        )
-        completionHandler()
     }
 
     // MARK: - URL & Universal Links (Capacitor)
